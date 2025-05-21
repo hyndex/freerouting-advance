20edb1fe8d7d6d0b20cf6147840972f33556d59f
Chinmoy Bhuyan <b17cs082@cit.ac.in>
2025-05-21 21:53:15 +0530

    Document stop flags and add interruption tests

--- src/main/java/app/freerouting/core/StoppableThread.java
+++ src/main/java/app/freerouting/core/StoppableThread.java
@@ -7,8 +7,17 @@ import app.freerouting.datastructures.Stoppable;
  */
 public abstract class StoppableThread extends Thread implements Stoppable
 {
+  /**
+   * Indicates that the entire routing job should stop. When this flag is set the
+   * autorouter as well as any subsequent optimizer passes will be aborted.
+   */
   private boolean stop_requested = false;
-  // TODO: why do we need this, can't we use stop_requested?
+
+  /**
+   * Indicates that only the autorouter should be aborted. The optimizer is still
+   * allowed to continue running. {@code stop_requested} implies this flag but it
+   * can be set independently when only the autorouter needs to stop.
+   */
   private boolean stop_auto_router = false;
 
   /**
@@ -26,7 +35,7 @@ public abstract class StoppableThread extends Thread implements Stoppable
     thread_action();
   }
 
-  // Request the thread to stop including the fanout, auto-router and optimizer tasks
+  // Request the thread to stop including the fanout, autorouter and optimizer tasks
   @Override
   public synchronized void requestStop()
   {
@@ -40,13 +49,18 @@ public abstract class StoppableThread extends Thread implements Stoppable
     return stop_requested;
   }
 
-  // Request the thread to stop the auto-router, but continue with the optimizer and other tasks
+  /**
+   * Request the thread to stop only the autorouter. The optimizer and other
+   * tasks are allowed to continue.
+   */
   public synchronized void request_stop_auto_router()
   {
     stop_auto_router = true;
   }
 
-  // Check if the thread should stop the auto router
+  /**
+   * Returns {@code true} if the autorouter should be aborted.
+   */
   public synchronized boolean is_stop_auto_router_requested()
   {
     return stop_auto_router;
